833c89146adee126c4e75e0d1e251b0b2142d26a
Caestecker <sebastien.caestecker@gmail.com>
2025-05-20 14:59:36 +0200

    Create marge.sql

---
+++ marge.sql
@@ -0,0 +1,14 @@
+WITH ca_category AS(
+SELECT
+  p.category,
+  sum(p.cost) as cout,
+  sum(p.retail_price) as ca
+FROM bigquery-public-data.thelook_ecommerce.order_items oi
+JOIN bigquery-public-data.thelook_ecommerce.products p on p.id=oi.product_id
+WHERE oi.status='Complete' and date(oi.created_at)>=date_sub(current_date(),INTERVAL 4 month)
+group by 1
+)
+Select 
+  category,
+  round(((ca-cout)/ca)*100,2) as marge
+FROM ca_category
